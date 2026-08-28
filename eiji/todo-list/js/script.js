2fbd0e476d9fdbc727dd9b31a9a541a6c35ac15b
eiji-hbrs <eiji.haubursin@outlook.com>
2026-08-28 12:13:37 +0200

    first commit

---
+++ eiji/todo-list/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".btn").click(function(){
+        var todoInput = $("#todoInput").val();
+    )};
+)};
+
+if (todoInput = ""){
+    $("#message").text("Veuillez saisir une tâche.");
+}
+
